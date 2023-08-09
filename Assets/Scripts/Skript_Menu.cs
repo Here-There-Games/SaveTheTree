8da826b2c89f7251b68f1a2d204462abd9130f35
Roma5640 <138190068+Roma5640@users.noreply.github.com>
2023-08-09 12:38:58 -0700

    MainMenu

---
+++ Assets/Scripts/Skript_Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skript_Menu : MonoBehaviour
+
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
