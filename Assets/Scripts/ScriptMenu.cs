d89f07d0012bb37382f6a35c08779b0605557eb9
Domiurk <nikegoof@gmail.com>
2023-08-10 22:50:49 +0300

    Update

--- Assets/Scripts/ScriptMenu.cs
+++ Assets/Scripts/ScriptMenu.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Skript_Menu : MonoBehaviour
-
+public class ScriptMenu : MonoBehaviour
 {
     public void LoadLevel()
     {
