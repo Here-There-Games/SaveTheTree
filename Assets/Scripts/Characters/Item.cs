ef0c8ccb4add4b43e9d1dd2d7f213c77987e3e2a
Domiurk <nikegoof@gmail.com>
2023-09-09 22:41:59 +0300

    Add particles, animation water drop

--- Assets/Scripts/Characters/Item.cs
+++ Assets/Scripts/Characters/Item.cs
@@ -6,6 +6,12 @@ namespace Entity
     public class Item : MonoBehaviour, IFloat
     {
         [field: SerializeField] public float Value { get; private set; }
+        [SerializeField] private ParticleSystem showParticle;
+
+        private void Start()
+        {
+            showParticle?.Play();
+        }
 
         public void SetExperience(float value)
         {
